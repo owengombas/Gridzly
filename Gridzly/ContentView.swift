273eed7967b04535f280601eb5ce8970926e9efd
Owen Calvin <owen.gombas@gmail.xom>
2020-02-08 20:29:25 +0100

    Initial Commit

---
+++ Gridzly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gridzly
+//
+//  Created by Owen on 08.02.20.
+//  Copyright © 2020 ven. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
